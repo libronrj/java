8ab53a19f6dc922ce5cc246236b5f42c9981e563
libron <libronjohn@gmail.com>
2025-01-26 13:57:04 +0530

    ''

--- string/PrintAlphabetAndOccurence.java
+++ string/PrintAlphabetAndOccurence.java
@@ -1,20 +1,23 @@
 import java.util.Map;
 import java.util.HashMap;
 
-public class PrintAlphabetsAndOccurence {
+public class PrintAlphabetAndOccurence {
 
     public static void main(String[] args) {
 
         String str = "aaaabbbccd";
 
         Map<Character, Integer> map = new HashMap<>();
+
+        // Count the occurence as store te value for each key where key is the character
         for (int i = 0; i < str.length(); i++)
             map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
 
-        // Print alphabet then number
+        // Print alphabet then number using lambda expression
         map.forEach((key, value) -> System.out.print(key + "" + value));
         System.out.println();
-        // Print number then alphabet
+
+        // Print number then alphabet using lambda expression
         map.forEach((key, value) -> System.out.print(value + "" + key));
         System.out.println();
     }
