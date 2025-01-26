8f4e91f9b199a28ad65c9ec250017aae3013d213
libron <libronjohn@gmail.com>
2025-01-26 14:07:49 +0530

    ''

--- string/PrintSubString.java
+++ string/PrintSubString.java
@@ -1,8 +1,11 @@
 public class PrintSubString {
     public static void main(String[] args) {
-
+        // Take user input
         String str = "Before 6 a.m (66789abcv), meet me";
+
+        // Extract partial string from existing string using substring() and indexOf()
         String newStr = str.substring(str.indexOf("(") + 1, str.indexOf(")"));
+
         System.out.println(newStr);
     }
 }
