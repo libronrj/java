2b064f51d18bc8021f19ced2bc20125f4c793ae2
libron <libronjohn@gmail.com>
2025-01-26 14:04:36 +0530

    ''

--- string/PrintDenominations.java
+++ string/PrintDenominations.java
@@ -2,18 +2,24 @@ public class PrintDenominations {
 
     public static void main(String[] args) {
 
+        // Store currency notes in an array
         int[] array = { 2000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
 
-        int amount = 5000;
-        int quotient = 0;
+        // Input amount to get denomination
+        int amount = 10564;
 
+        // Count the number of notes for denomination
+        int noOfNotes = 0;
+
+        // Loop to traverse through the collection of currency
         for (int i = 0; i < array.length; i++) {
 
-            quotient = amount / array[i];
+            noOfNotes = amount / array[i];
             amount = amount % array[i];
 
-            if (i < array.length && quotient != 0)
-                System.out.println(array[i] + " x " + quotient);
+            // Condition to print the currency and notes required
+            if (i < array.length && noOfNotes != 0)
+                System.out.println(array[i] + " x " + noOfNotes);
         }
     }
 }
