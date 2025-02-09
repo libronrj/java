dcbbe4f86b15a3231a6e7425068a1220afcdcf10
libron <libronjohn@gmail.com>
2025-02-09 13:15:41 +0530

    ''

--- array/ListContinuousRange.java
+++ array/ListContinuousRange.java
@@ -6,40 +6,37 @@ import java.util.List;
 public class ListContinuousRange {
 
     public static void main(String[] args) {
-
-        int[] array = { 0, 2, 3, 4, 6, 8, 9 };
+        int[] array = { 0, 1, 3, 4, 6, 8, 9 };
         List<String> result = listContinuousRange(array);
         System.out.println(result);
-
     }
 
     public static List<String> listContinuousRange(int[] array) {
-
         List<String> list = new ArrayList<>();
 
-        int n = array.length;
+        if (array == null || array.length == 0)
+            return list;
 
-        int start = 0;
-        int end = 0;
+        int start = array[0];
 
-        for (int i = 0; i < n - 1; i++) {
-
-            start = array[i];
-
-            for (int j = i + 1; j < n; j++) {
-
-                end = array[j];
-
-                if (i == 0 && start + 1 != end)
-                    list.add("" + start);
-                else if (start + 1 == end)
-                    continue;
-                else
-                    list.add("" + start + "->" + end);
-
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[i - 1] + 1) { // If not consecutive, close the range
+                addRange(list, start, array[i - 1]);
+                start = array[i]; // Start new range
             }
         }
+
+        // Add the last range
+        addRange(list, start, array[array.length - 1]);
+
         return list;
     }
 
+    private static void addRange(List<String> list, int start, int end) {
+        if (start == end) {
+            list.add(String.valueOf(start));
+        } else {
+            list.add(start + "->" + end);
+        }
+    }
 }
